433b5ab327b8ad3b4bed4b2af7fb9351fbd53044
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-05-23 20:32:50 +0700

    create 'what we do' and 'what we work' section in home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import mdi_android from "../assets/icons/mdi_android.svg";
+import mdi_web from "../assets/icons/mdi_web.svg";
+import mdi_view_quilt from "../assets/icons/mdi_view_quilt.svg";
+
+const products = ref([
+  {
+    icon: mdi_android,
+    title: "Mobile Application",
+    detail: `Building mobile applications is never easy. P2P lending app?
+              Logistic App? E-Commerce App? We have done them all. Done
+              them all well. Done them all fast.`,
+  },
+  {
+    icon: mdi_web,
+    title: "Web Application",
+    detail: `We can build any web applications you need.
+              A personal blog? Company Profile? E-commerce?
+              or any specific functionalities ? We got you covered!
+              We have a strong team of web developers that can
+              follow your stack and needs.`,
+  },
+  {
+    icon: mdi_view_quilt,
+    title: "UI/UX Design",
+    detail: `All things are designed. A few things are designed well.
+              We try to elevate your ideas with great UI/UX design,
+              suited to your taste, addressing the right pain points.`,
+  },
+]);
+</script>
 
 <template>
-  <main class="font-poppins">
+  <main class="overflow-hidden font-poppins">
     <section class="bg-[#F8F8F8] pt-10 pb-10 lg:pb-0">
       <div
         class="container flex flex-col justify-between lg:flex-row lg:items-center"
@@ -44,6 +75,75 @@
             alt=""
           />
         </div>
+      </div>
+    </section>
+
+    <section class="container py-10 lg:px-20 lg:py-32 xl:px-52">
+      <div class="flex flex-col items-center justify-between md:flex-row">
+        <div>
+          <div class="relative overflow-hidden rounded-xl drop-shadow-xl">
+            <img
+              src="../assets/images/section-1-image.png"
+              alt="section-1-image"
+            />
+            <div class="absolute top-0 right-0 bg-white px-8 py-4">
+              <a
+                class="text-black transition duration-300 ease-in-out hover:text-[#9F9F9F]"
+                href="#"
+                >View Detail</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="mt-6 md:basis-[40%] lg:mt-0 lg:basis-[45%]">
+          <div>
+            <h2 class="mb-7 text-4xl font-semibold text-black-light lg:ml-7">
+              What We Do?
+            </h2>
+            <div>
+              <div
+                v-for="product in products"
+                :key="product"
+                class="mb-5 flex items-start"
+              >
+                <img :src="product.icon" alt="" class="mr-5 mt-[2px]" />
+                <div>
+                  <h6 class="mb-1 text-sm font-semibold text-black-light">
+                    {{ product.title }}
+                  </h6>
+                  <p class="text-xs font-normal leading-6 text-[#5E5B5B]">
+                    {{ product.detail }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container py-10 lg:py-32 lg:pr-0 lg:pl-20 xl:pl-52">
+      <div class="relative flex">
+        <div
+          class="left-0 z-10 w-full bg-white p-8 drop-shadow-xl md:p-12 lg:absolute lg:w-[50%]"
+        >
+          <h2 class="mb-4 text-4xl font-semibold text-black-light">
+            How We Work
+          </h2>
+          <p class="mb-4 font-medium text-[#9F9F9F]">
+            Let's discuss what projects you have in mind
+          </p>
+          <button
+            class="bg-[#4A4A4A] px-6 py-4 text-sm font-medium text-white transition duration-300 ease-in-out hover:bg-[#606060]"
+          >
+            Let’s Have a Coffee
+          </button>
+        </div>
+        <img
+          class="relative z-0 hidden lg:left-[24.5rem] lg:-top-20 lg:block"
+          src="../assets/images/section-2-image.png"
+          alt=""
+        />
       </div>
     </section>
   </main>
